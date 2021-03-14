2fc069c0263c724e1016cf0d00e2cbde9b1a12f2
Benjamin Holmes <ben.holmes1357@gmail.com>
2021-03-13 20:02:41 -0800

    Update cosmosQs.py

--- cosmosQs.py
+++ cosmosQs.py
@@ -11,14 +11,14 @@ client = CosmosClient(endpoint, key)
 
 # Create a database
 # <create_database_if_not_exists>
-database_name = 'AzureSampleFamilyDatabase'
+database_name = 'AzureReservationDatabase'
 database = client.create_database_if_not_exists(id=database_name)
 # </create_database_if_not_exists>
 
 # Create a container
 # Using a good partition key improves the performance of database operations.
 # <create_container_if_not_exists>
-container_name = 'FamilyContainer'
+container_name = 'ReservationContainer'
 container = database.create_container_if_not_exists(
     id=container_name, 
     partition_key=PartitionKey(path="/lastName"),
@@ -28,17 +28,17 @@ container = database.create_container_if_not_exists(
 
 
 # Add items to the container
-family_items_to_create = [reservationData.get_andersen_family_item(), reservationData.get_johnson_family_item(), reservationData.get_smith_family_item(), reservationData.get_wakefield_family_item()]
+reservation_items_to_create = [reservationData.get_andersen_family_item(), reservationData.get_johnson_family_item(), reservationData.get_smith_family_item(), reservationData.get_wakefield_family_item()]
 
  # <create_item>
-for family_item in family_items_to_create:
-    container.create_item(body=family_item)
+for reservation_item in reservation_items_to_create:
+    container.create_item(body=reservation_item)
 # </create_item>
 
 # Read items (key value lookups by partition key and id, aka point reads)
 # <read_item>
-for family in family_items_to_create:
-    item_response = container.read_item(item=family['id'], partition_key=family['lastName'])
+for reservation in reservation_items_to_create:
+    item_response = container.read_item(item=reservation['id'], partition_key=reservation['lastName'])
     request_charge = container.client_connection.last_response_headers['x-ms-request-charge']
     print('Read item with id {0}. Operation consumed {1} request units'.format(item_response['id'], (request_charge)))
 # </read_item>
